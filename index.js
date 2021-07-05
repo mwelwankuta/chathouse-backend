1fc8dddaaf816a225301b0f7f8fa0a0b37302b16
Mwelwa Nkuta <mwelwankuta@mail.com>
2021-07-05 18:22:45 +0200

    fixed port

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
-const PORT = process.env.port || 3002
+const PORT = process.env.PORT || 3002
 
 const server = app.listen(PORT, () => {
   console.log("Server Running on Port 3002...")
@@ -19,5 +19,5 @@ const io = new Server(server, {cors: {origin: "*"}})
 IOConnection(io) // socket.io 
 
 app.get('/', (req, res) => {
-  res.status(200).send('Server running ' +PORT)
+  res.status(200).send('Server running ')
 })
