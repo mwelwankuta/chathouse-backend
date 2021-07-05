819daecc6dd06b7063751bce68fcc13fe1008cdf
Mwelwa Nkuta <mwelwankuta@mail.com>
2021-07-05 18:20:15 +0200

    added port number

--- index.js
+++ index.js
@@ -8,10 +8,16 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
-const server = app.listen("3002", () => {
+const PORT = process.env.port || 3002
+
+const server = app.listen(PORT, () => {
   console.log("Server Running on Port 3002...")
 })
 
 const io = new Server(server, {cors: {origin: "*"}})
 
 IOConnection(io) // socket.io 
+
+app.get('/', (req, res) => {
+  res.status(200).send('Server running ' +PORT)
+})
